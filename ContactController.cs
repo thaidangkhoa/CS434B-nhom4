5b6698a6655c6ceb81a9bd276d753ebece3e89ed
MinhHoan <hoanvongunguoi@gmail.com>
2025-05-20 23:11:45 +0700

    Nhap

---
+++ ContactController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace YourApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        // Hiển thị trang liên hệ
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Xử lý gửi thông tin liên hệ
+        [HttpPost]
+        public ActionResult Send(string Name, string Email, string Message)
+        {
+            // TODO: xử lý gửi email hoặc lưu vào database
+
+            TempData["Success"] = "Cảm ơn bạn đã liên hệ! Chúng tôi sẽ phản hồi sớm.";
+            return RedirectToAction("Index");
+        }
+    }
+}
